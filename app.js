3a1c1351f46a2a54d642b811fc6ee0c01e4bbb0f
Grabiel <grabiel.neyra@davinci.edu.ar>
2026-05-07 18:00:13 -0300

    Primer Parcial

---
+++ app.js
@@ -0,0 +1,53 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            buscar: "",
+            juegos: [],
+            cargando: true
+        }
+
+    },
+    computed: {
+
+    },
+    methods: {
+        verDesc(juego) {
+            juego.mostrarDesc = !juego.mostrarDesc
+        },
+        eliminarJuego(id) {
+            this.juegos = this.juegos.filter(juego => juego.id !== id)
+        },
+        fav(juego) {
+            juego.favorito = !juego.favorito
+            const juegoFav = this.juegos.filter(j => j.favorito).map(j => j.id)
+            localStorage.setItem('favoritos', JSON.stringify(juegoFav))
+
+        }
+
+    },
+    async mounted() {
+        try {
+
+            const response = await fetch('./data.json')
+            const data = await response.json()
+            this.juegos = data.map(item => ({
+                ...item,
+                mostrarDesc: false,
+                favorito: false
+            }))
+            this.cargando = false
+            const idsFavoritos = JSON.parse(localStorage.getItem('favoritos'))
+            if (idsFavoritos) {
+                this.juegos.forEach(juego => {
+                    juego.favorito = idsFavoritos.includes(juego.id)
+                })
+            }
+        } catch (error) {
+            console.error('Error al acargar los datos: ', error)
+            this.cargando = false
+        }
+
+    }
+})
+
+app.mount("#app")
